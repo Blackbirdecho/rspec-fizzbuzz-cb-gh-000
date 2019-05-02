4c878ecae2e58a6807e1a8a8dafd7b8dab3ea8fe
Khaled Ali Ahmed <s.echo.bird@gmail.com>
2019-05-02 17:51:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,11 @@
 # See README.md for instructions on how to do this
 require_relative '../fizzbuzz.rb'
 def fizzbuzz
-
+  if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz" # Go fizz
+    end
 end
+
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz()
